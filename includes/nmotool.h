f56ea9a3ed24433d1e5e6ba5bffe9ca4204c2867
Plastic <plastic@Plastics-Mac.local>
2014-04-24 13:52:39 -0700

    Pas fier

--- includes/nmotool.h
+++ includes/nmotool.h
@@ -13,11 +13,23 @@
 #ifndef NMOTOOL_H
 # define NMOTOOL_H
 
+# include <mach-o/loader.h>
+
 typedef enum	e_bool
 {
 	FALSE,
 	TRUE
 }				t_bool;
+
+typedef struct s_bin t_bin;
+
+t_bin
+{
+	struct mach_header_64	*header;
+	uint64_t				size_text;
+	uint32_t				off_text;
+	t_bin					*next;
+}
 
 void			print_ptr_to_hex(size_t ptr, t_bool prefix, t_bool full);
 void			print_byte_to_hex(unsigned char byte);
--- sources/ft_otool.c
+++ sources/ft_otool.c
@@ -26,61 +26,40 @@ int		error_printer(char *str, int fd)
 	return (1);
 }
 
-size_t	size_of_file(int fd)
-{
-	struct stat	stat;
-
-	fstat(fd, &stat);
-	return (stat.st_size);
-}
-
 int		ft_otool(void *ptr, size_t size)
 {
 	struct fat_header		*fat;
 	struct mach_header_64	*mach;
-	size_t					size;
+	t_bin					*list;
 
-	size = 0;
 	fat = (struct fat_header *)ptr;
-	if (fat->magic == FAT_MAGIC)
-	{
-		ft_putendl("Is a fat binary - Big endian");
-		size = fat->size
-	}
+	mach = (struct mach_header_64 *)ptr;
 	if (fat->magic == FAT_CIGAM)
 	{
-		ft_putendl("Is a fat binary - Little endian");
+		add_header();
 	}
-	mach = (struct mach_header_64 *)ptr;
-	if (mach->magic == MH_MAGIC_64)
-	{
-		ft_putendl("Is a binary - Big endian");
-	}
-	if (mach->magic == MH_CIGAM_64)
-	{
-		ft_putendl("Is a binary - Little endian");
-	}
-	(void)size;
+	else if (mach->magic == MH_CIGAM_64)
+
 	return (0);
 }
 
 int		main(int ac, char **av)
 {
-	void	*ptr;
-	size_t	size;
-	int		ret;
-	int		fd;
+	struct stat	stat;
+	void		*ptr;
+	int			ret;
+	int			fd;
 
 ret = 0;
 	if (ac != 2)
 		return (error_printer("usage ./otool binary", 0));
 	if ((fd = open(av[1], O_RDONLY)) < 2)
 		return (error_printer("error: file openning impossible", 0));
-	size = size_of_file(fd);
-	if ((ptr = mmap(0, size, PROT_READ, MAP_SHARED, fd, 0)) == (void *)-1)
+	fstat(fd, &stat);
+	if ((ptr = mmap(0, stat.st_size, PROT_READ, MAP_SHARED, fd, 0)) == (void *)-1)
 		return (error_printer("error: file maping impossible", fd));
-	ft_otool(ptr, size);
-	if (munmap(ptr, size) == -1)
+	ft_otool(ptr, stat.st_size);
+	if (munmap(ptr, stat.st_size) == -1)
 		return (error_printer("error: file unmaping impossible", fd));
 	if (close(fd) == -1)
 		return (error_printer("error: file closing impossible", 0));
